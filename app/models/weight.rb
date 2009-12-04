1bc2945d3ab91d55d7ac2295808bc0d87d0f0b34
pbalduino <pbalduino+github@gmail.com>
2009-12-04 15:32:25 -0200

    Fixed view

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,4 +1,4 @@
 class Weight < ActiveRecord::Base
   validates_numericality_of :weight, :message => "O peso deve ser um número e os decimais devem ser separados por ponto"
-  validates_size_of :weight, :minimum => 1, :maximum => 200, :message => "O peso deve ser maior que zero e menor que 200 quilos"
+  validates_size_of :weight, :within => 1..200, :message => "O peso deve ser maior que zero e menor que 200 quilos"
 end
