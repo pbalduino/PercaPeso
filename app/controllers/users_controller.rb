0862641b428a93479d5ab15b189ae0d1142b9be5
pbalduino <pbalduino+github@gmail.com>
2009-12-08 13:33:44 -0200

    Fixed Chrome error when submiting user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   def ajax_update
     @user = User.find(session[:user_id])
-    @user[params[:field].sub('#user_', '')] = params[:value]
+    @user[params[:field].sub('#', '')] = params[:value]
     @user.save!
 
     render :json => 'ok', :status => 200
