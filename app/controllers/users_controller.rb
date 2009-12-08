751fa969a6eea86fd3720cef929cc798e7178628
pbalduino <pbalduino+github@gmail.com>
2009-12-08 11:53:57 -0200

    Reversed date order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(session[:user_id])
-    @weights = Weight.find(:all, :order => :date, :limit => 5, :conditions => {:user_id => session[:user_id]})
+    @weights = Weight.find(:all, :order => 'date DESC', :limit => 5, :conditions => {:user_id => session[:user_id]})
   end
 
   def ajax_update
