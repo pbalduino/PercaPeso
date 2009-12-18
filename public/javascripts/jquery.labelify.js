e342075fff51c7f52282190aa0062998bac2e8f7
pbalduino <pbalduino+github@gmail.com>
2009-12-18 13:47:40 -0200

    Added datepicker to profile screen

--- public/javascripts/jquery.labelify.js
+++ public/javascripts/jquery.labelify.js
@@ -3,28 +3,40 @@ function applyClick() {
     data['field'] = $(this).attr('associate');
     data['value'] = $($(this).attr('associate')).attr('value');
     $.post('/profile_update', data, null, 'json');
-    $(this).attr('src', '/images/loading.gif');
-    $($(this).attr('associate')).removeClass('labelified_active');
-    $($(this).attr('associate')).addClass('labelified');
+
     $($(this).attr('associate')).attr('read_only', true);
     $($(this).attr('associate')).bind('click', onClick);
     $($(this).attr('associate')).bind('click', onClick);
     $($(this).attr('associate')).bind('mouseover', onMouseOver);
     $($(this).attr('associate')).bind('mouseleave', onMouseLeave);
     $($(this).attr('associate')).bind('mouseout', onMouseLeave);
+    if ($($(this).attr('associate')).attr('id') == 'date') {
+        $($(this).attr('associate')).removeClass('date-pick');
+        $('.dp-choose-date').remove();
+    }
+    $($(this).attr('associate')).removeClass('labelified_active');
+    $($(this).attr('associate')).addClass('labelified');
+    // document.removeChild(document.getElementById($(this).attr('id')));
     $(this).remove();
 }
 
 function onClick() {
-    $(this).attr('read_only', false);
     $(this).removeClass('labelified');
-    $(this).addClass('labelified_active');
     $(this).unbind('click', null);
     $(this).unbind('mouseover', null);
     $(this).unbind('mouseleave', null);
     $(this).unbind('mouseout', null);
     $('#edit_' + $(this).attr('id')).remove();
     $(this).after('<img src="/images/apply_icon.png" width="15px" class="apply" associate="#' + $(this).attr('id') + '">')
+    console.log($(this).attr('id'));
+    if ($(this).attr('id') == 'date') {
+        $(this).addClass('date-pick');
+        $('.date-pick').datePicker({startDate:'01/11/2009'});
+        $(this).attr('read_only', true);
+    } else {
+        $(this).attr('read_only', false);
+        $(this).addClass('labelified_active');
+    }
 }
 
 function onMouseOver() {
